55699f4c9ca3c01f7b414e978aeb74e749005442
Rolazar Aberin <rolazaraberin@gmail.com>
2023-11-28 12:09:35 -0800

    Renamed for Jest testing

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     // "@babel/preset-react",
-    // "@babel/preset-typescript",
+    "@babel/preset-typescript",
   ],
   // plugins: [
   //   ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: false }],
