be547f3959f8e598f925899b3b7d6dbe7b6b8a4a
Rolazar Aberin <rolazaraberin@gmail.com>
2023-11-28 14:48:38 -0800

    Removed unused files

--- lambda/expressServerless.ts
+++
@@ -1,5 +0,0 @@
-import { Handler } from "@netlify/functions";
-import expressApp from "../src/expressApp";
-import serverless from "serverless-http";
-
-export const handler = serverless(expressApp) as Handler;
