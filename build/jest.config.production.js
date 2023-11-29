813574878312910b5aaf756a7d21e2c0be5e3087
Rolazar Aberin <rolazaraberin@gmail.com>
2023-11-29 00:15:27 -0800

    New build

---
+++ build/jest.config.production.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    rootDir: "build",
+    testPathIgnorePatterns: ["bak"],
+};
