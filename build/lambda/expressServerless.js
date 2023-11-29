38c510d032aab9962f0a9aaf1d04eff7b6710f7e
Rolazar Aberin <rolazaraberin@gmail.com>
2023-11-29 00:15:18 -0800

    Removed unused files

--- build/lambda/expressServerless.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const expressApp_1 = __importDefault(require("../src/expressApp"));
-const serverless_http_1 = __importDefault(require("serverless-http"));
-exports.handler = (0, serverless_http_1.default)(expressApp_1.default);
